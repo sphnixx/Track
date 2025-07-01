a12a5e9e635afba0f50be90e9d4ce4ea8bef3849
sphnixx <anonymoussphix@gmail.com>
2025-07-01 17:52:59 +0530

    Added new gradient for landing page

--- lib/core/Colors/colors.dart
+++ lib/core/Colors/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import '../Aligments/aligment.dart';
 
 class AppColors {
-  static const Color background = Color(0xFF0F0F0F);        // Jet black
-  static const Color greyDark = Color(0xFF1E1E1E);          // Deep grey
-  static const Color accentGreen = Color(0xFF00FF9C);       // Neon green
-  static const Color accentPurple = Color(0xFF9C4DFF);      // Purple
-  static const Color textPrimary = Colors.white;            // Main white text
-  static const Color textSecondary = Color(0xFFB0B0B0);      // Light grey text
+  static const Color darkBlack = Color(0xFF0A0A0A); // Deep black
+  static const Color matteGrey = Color(0xFF1E1E1E); // iOS matte grey
+  static const Color darkGreen = Color(0xFF0F3D2E); // Rich dark green
+  static const Color luxorGreen = Color(0xFF1C5D42); // Deep luxury green
+  static const Color metallicGrey = Color(0xFF2C2C2E); // Premium metallic shade
 }
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:track/presentation/screens/starting_screen.dart';
+
+void main() {
+  runApp(StartingScreen());
+}
